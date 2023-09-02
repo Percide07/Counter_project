061972bf28f55533336ff9ab64cc6f4bfbe2f925
Percide07 <percidehonhonou@gmail.com>
2023-09-02 12:28:16 +0100

    adding another Js properties to change the result

--- script.js
+++ script.js
@@ -7,25 +7,40 @@ const plus = document.querySelector('#inc');
 const screen = document.querySelector('#screen');
         // Operation
 minus.addEventListener('click',function(){
-    screen.innerText = screen.innerText -1;
+    screen.innerText = Math.floor(screen.innerText) - 1;
     // screen.append(myValue-=1);
     if (screen.innerText < 0) {
-        screen.style.color = 'red';
+        // screen.style.color = 'red';
+        screen.classList.add('add1');
+        screen.classList.remove('add2');
+        screen.classList.remove('add3');
     } 
     else if (screen.innerText <= 0) {
-        screen.style.color = 'black';
+        // screen.style.color = 'black';
+        screen.classList.add('add2');
+        screen.classList.remove('add3');
+
     }
 })       
 rest.addEventListener('click',function(){
     screen.innerHTML = 0;
-    screen.style.color = 'black';
+    // screen.style.color = 'black';
+    screen.classList.add('add2');
+    // screen.classList.remove('add1');
+    screen.classList.remove('add3');
+
 }) 
 plus.addEventListener('click',function(){
-    screen.innerText = Number(screen.innerText) +1;
+    screen.innerText = Math.floor(screen.innerText) + 1;
     if (screen.innerText > 0) {
-        screen.style.color = 'green';
+        // screen.style.color = 'green';
+        screen.classList.remove('add1');
+        screen.classList.add('add3');
+
     } 
     else if (screen.innerText >= 0) {
-        screen.style.color = 'black';
+        // screen.style.color = 'black';
+        screen.classList.remove('add3');
+        screen.classList.add('add2');
     }
 }) 
